c8fe9c3e051b3cff112703181b8f15b0eaf2b001
dylanbaghel <baghelabhi39@gmail.com>
2019-02-24 15:23:03 +0530

    extract prisma secret and set the management api secret

--- src/prisma.js
+++ src/prisma.js
@@ -6,7 +6,7 @@ const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
     fragmentReplacements,
-    secret: "abhishek"
+    secret: process.env.PRISMA_SECRET
 });
 
 export { prisma as default };
